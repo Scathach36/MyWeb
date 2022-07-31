66509a96dc2f5d2dd05ebf94afe4b898e47e38ce
scathach <scathach0822@hotmail.com>
2022-07-31 21:26:41 +0800

    微调结构，删除多余的组件

--- shopcar_demo/src/App.vue
+++ shopcar_demo/src/App.vue
@@ -1,20 +1,32 @@
 <template>
   <div>
-    <ShopcarHeader backgroundColor="red" color="white" title="购物车"></ShopcarHeader>
-    <ShopcarMain></ShopcarMain>
+    <ShopcarHeader
+      backgroundColor="red"
+      color="white"
+      title="购物车"
+    ></ShopcarHeader>
+    <div class="main">
+      <ShopcarItem></ShopcarItem>
+    </div>
     <ShopcarFooter></ShopcarFooter>
   </div>
 </template>
 
 <script>
 import ShopcarHeader from "./components/Shopcar_Header.vue";
-import ShopcarMain from "./components/Shopcar_Main.vue";
 import ShopcarFooter from "./components/Shopcar_Footer.vue";
+import ShopcarItem from "./components/Shopcar_item.vue";
 
 export default {
-  components: {ShopcarHeader, ShopcarMain, ShopcarFooter},
+  components: { ShopcarHeader, ShopcarFooter, ShopcarItem },
 };
 </script>
 
-<style>
+<style lang="less" scoped>
+.main {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding-top: 40px;
+}
 </style>
--- shopcar_demo/src/main.js
+++ shopcar_demo/src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
+import { createApp, VueElement } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://www.escook.cn'
+VueElement.prototype.$axios = axios
 
 createApp(App).mount('#app')
